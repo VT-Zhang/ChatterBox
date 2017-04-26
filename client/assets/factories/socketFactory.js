c40a541e59c5a14851c0ff41c216debde67d0fc9
Nishant Aggarwal <nish8192@gmail.com>
2017-04-26 14:32:57 -0400

    Working on sockets

--- client/assets/factories/socketFactory.js
+++ client/assets/factories/socketFactory.js
@@ -2,6 +2,10 @@ app.factory('socketFactory', ['$http', function($http) {
 
   function SocketFactory(){
   	var socket = io.connect();
+    socket.emit('test', {message: "trying"})
+    socket.on('success', function(data){
+        console.log(data.message);
+    })
 
   return new SocketFactory();
 }]);
--- sockets.js
+++ sockets.js
@@ -1,7 +1,15 @@
+var users = require('./server/controllers/users.js')
+var messages = require('./server/controllers/messages.js')
+var channels = require('./server/controllers/channels.js')
+var conversations = require('./server/controllers/conversations.js')
+
 module.exports = function(server){
     var io = require('socket.io').listen(server);
     io.sockets.on('connection', function (socket) {
         console.log("Sockets are enabled on this application!");
         console.log(socket.id);
+        socket.on('test', function(data){
+            io.emit('success', {message: "Test is successful!"});
+        });
     });
 }
