5a2862122f33a788b27dd91aec7ebb83cad52633
VT-Zhang <jianz@vt.edu>
2017-04-25 23:48:21 -0400

    Added Bootstrap dependencies.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express  = require( 'express' ),
 // error two
 app.use( express.static( path.join( __dirname, 'client' )));
 app.use( express.static( path.join( __dirname, 'bower_components' )));
+app.use( express.static( path.join( __dirname, 'node_modules' )));
 app.use( bp.json() );
 require('./server/config/mongoose.js')
 require("./server/config/routes.js")(app);
